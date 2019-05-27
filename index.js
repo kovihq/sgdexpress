9295cff0fdf42b89a336f0c5717785f1c304ed01
costajoao <joao@joaocosta.com>
2019-05-27 00:43:06 -0300

    Remove promisify

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-import { promisify } from 'util';
-import { parseString } from 'xml2js';
-import got from 'got';
-import { xmlConductor, xmlVehicle, url, clean, valid, cleanCnh, validCnh } from './lib/util';
-import DetranError from './lib/error';
-import mapError from './lib/mapErrors';
+import { parseString } from "xml2js";
+import got from "got";
+import {
+  xmlConductor,
+  xmlVehicle,
+  url,
+  clean,
+  valid,
+  cleanCnh,
+  validCnh
+} from "./lib/util";
+import DetranError from "./lib/error";
+import mapError from "./lib/mapErrors";
 
 class Service {
   constructor(code, user, pass) {
     this.code = code;
     this.user = user;
     this.pass = pass;
-    this._parseString = promisify(parseString);
     this.headers = {
-      'Content-Type': 'text/xml',
+      "Content-Type": "text/xml"
     };
   }
 
@@ -21,8 +27,11 @@ class Service {
     try {
       const body = xmlVehicle(this.code, this.user, this.pass, plate);
       const res = await got.post(url.vehicle, { headers: this.headers, body });
-      const resParse = await this._parseString(res.body, { explicitArray: false, trim: true });
-      const access = clean(resParse['soap:Envelope']['soap:Body']);
+      const resParse = await this._parseString(res.body, {
+        explicitArray: false,
+        trim: true
+      });
+      const access = clean(resParse["soap:Envelope"]["soap:Body"]);
       if (!access) return false;
       isValid = valid(access.query);
       if (isValid.status) return access.query;
@@ -41,9 +50,15 @@ class Service {
     let isValid;
     try {
       const body = xmlConductor(this.code, this.user, this.pass, cpf, cnh);
-      const res = await got.post(url.conductor, { headers: this.headers, body });
-      const resParse = await this._parseString(res.body, { explicitArray: false, trim: true });
-      const access = cleanCnh(resParse['soap:Envelope']['soap:Body']);
+      const res = await got.post(url.conductor, {
+        headers: this.headers,
+        body
+      });
+      const resParse = await this._parseString(res.body, {
+        explicitArray: false,
+        trim: true
+      });
+      const access = cleanCnh(resParse["soap:Envelope"]["soap:Body"]);
       if (!access) return false;
       isValid = validCnh(access.query);
       if (isValid.status) return access.query;
